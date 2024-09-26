dc99a1b1257f3e917c4ef9357416de3827900f78
Utsav <utsavc317@gmail.com>
2024-09-26 22:47:48 +0530

    made history page mobile compatible

--- frontend/src/pages/Event/drep&req/frep&req/App.jsx
+++ frontend/src/pages/Event/drep&req/frep&req/App.jsx
@@ -1,66 +1,66 @@
-import React, { useState, useRef, useEffect } from "react";
-import Tabs from "./Tabs.jsx";
-import Request from "./Requests.jsx";
-import Report from "./Reports.jsx";
-const backendUrl =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api";
+  import React, { useState, useRef, useEffect } from "react";
+  import Tabs from "./Tabs.jsx";
+  import Request from "./Requests.jsx";
+  import Report from "./Reports.jsx";
+  const backendUrl =
+    import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api";
 
-const App = () => {
-  const [activeTab, setActiveTab] = useState("requests");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isSecondModalOpen, setIsSecondModalOpen] = useState(false);
-  const [requests, setRequests] = useState([]);
-  const [loading, setLoading] = useState(true);
-  
-  useEffect(() => {
-    // fetch data from API
-    const fetchRequests = async () => {
-      const response = await fetch(`${backendUrl}/requests`, {
-        method: "GET",
-        credentials: "include",
-      });
-      const data = await response.json();
-      setRequests(data);
-      setLoading(false);
-      // console.log(requests);
-      // console.log(data);
+  const App = () => {
+    const [activeTab, setActiveTab] = useState("requests");
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isSecondModalOpen, setIsSecondModalOpen] = useState(false);
+    const [requests, setRequests] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+      // fetch data from API
+      const fetchRequests = async () => {
+        const response = await fetch(`${backendUrl}/requests`, {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json();
+        setRequests(data);
+        setLoading(false);
+        // console.log(requests);
+        // console.log(data);
+      };
+      fetchRequests();
+    }, []);
+
+    // if (loading) {
+    //   return <div>Loading Requests...</div>;
+    // }
+
+    const [reports, setReports] = useState([]);
+    const [selectedRequest, setSelectedRequest] = useState(null);
+    const [selectedReport, setSelectedReport] = useState(null);
+    const modalRef = useRef(null);
+    const secondModalRef = useRef(null);
+
+    const handleTabClick = (tab) => {
+      setActiveTab(tab);
     };
-    fetchRequests();
-  }, []);
 
-  // if (loading) {
-  //   return <div>Loading Requests...</div>;
-  // }
-
-  const [reports, setReports] = useState([]);
-  const [selectedRequest, setSelectedRequest] = useState(null);
-  const [selectedReport, setSelectedReport] = useState(null);
-  const modalRef = useRef(null);
-  const secondModalRef = useRef(null);
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+    return (
+      <div className="bg-white min-h-screen p-4 rounded-xl dark:bg-gray-900">
+        <Tabs activeTab={activeTab} handleTabClick={handleTabClick} />
+        {activeTab === "requests" && (
+          <Request
+            requests={requests}
+            selectedRequest={selectedRequest}
+            setSelectedRequest={setSelectedRequest}
+          />
+        )}
+        {activeTab === "reports" && (
+          <Report
+            reports={reports}
+            selectedReport={selectedReport}
+            setSelectedReport={setSelectedReport}
+          />
+        )}
+      </div>
+    );
   };
 
-  return (
-    <div className="bg-white min-h-screen p-4 rounded-xl dark:bg-gray-900">
-      <Tabs activeTab={activeTab} handleTabClick={handleTabClick} />
-      {activeTab === "requests" && (
-        <Request
-          requests={requests}
-          selectedRequest={selectedRequest}
-          setSelectedRequest={setSelectedRequest}
-        />
-      )}
-      {activeTab === "reports" && (
-        <Report
-          reports={reports}
-          selectedReport={selectedReport}
-          setSelectedReport={setSelectedReport}
-        />
-      )}
-    </div>
-  );
-};
-
-export default App;
+  export default App;
