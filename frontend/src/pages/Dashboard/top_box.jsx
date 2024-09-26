a5d34d70d8afd3e1205976fb9258dab96f089834
Utsav <utsavc317@gmail.com>
2024-09-26 22:32:18 +0530

    fixed dashboard mobile view

--- frontend/src/pages/Dashboard/top_box.jsx
+++ frontend/src/pages/Dashboard/top_box.jsx
@@ -6,24 +6,24 @@ const TopBox = ({ accepted, rejected, modified, role, currentStatus }) => {
   return (
     <div className="w-full p-4 bg-blue-200 dark:bg-blue-800 rounded-lg"> 
       <div className="flex flex-col items-start w-full">
-        <div className="text-gray-900 dark:text-white text-5xl mb-4">Hi there, {role}</div>
-        <div className="text-gray-900 dark:text-white text-2xl mb-4">Current Status: {currentStatus}</div> 
-        <div className="flex flex-col md:flex-row w-full">
-          <div className="bg-green-500 text-white p-4 m-1 flex-1 rounded-lg hover:bg-green-700 dark:hover:bg-green-800 relative">
+        <div className="text-gray-900 dark:text-white text-3xl sm:text-5xl mb-4">Hi there, {role}</div>
+        <div className="text-gray-900 dark:text-white text-xl sm:text-2xl mb-4">Current Status: {currentStatus}</div> 
+        <div className="flex flex-col sm:flex-row w-full">
+          <div className="bg-green-500 text-white p-4 m-1 flex-1 rounded-lg hover:bg-green-700 dark:hover:bg-green-800 relative max-w-full sm:max-w-xs">
             <div className="flex items-center">
               <FontAwesomeIcon icon={faCheckCircle} className="mr-2 text-white" />
               <h3 className="text-lg font-bold">Accepted</h3>
             </div>
             <p>{accepted}</p>
           </div>
-          <div className="bg-red-500 dark:bg-red-600 text-white p-4 m-1 flex-1 rounded-lg hover:bg-red-700 dark:hover:bg-red-800 relative">
+          <div className="bg-red-500 dark:bg-red-600 text-white p-4 m-1 flex-1 rounded-lg hover:bg-red-700 dark:hover:bg-red-800 relative max-w-full sm:max-w-xs">
             <div className="flex items-center">
               <FontAwesomeIcon icon={faTimesCircle} className="mr-2 text-white" />
               <h3 className="text-lg font-bold">Rejected</h3>
             </div>
             <p>{rejected}</p>
           </div>
-          <div className="bg-orange-500 text-white p-4 m-1 flex-1 rounded-lg hover:bg-orange-700 dark:hover:bg-orange-800 relative">
+          <div className="bg-orange-500 text-white p-4 m-1 flex-1 rounded-lg hover:bg-orange-700 dark:hover:bg-orange-800 relative max-w-full sm:max-w-xs">
             <div className="flex items-center">
               <FontAwesomeIcon icon={faEdit} className="mr-2 text-white" />
               <h3 className="text-lg font-bold">Modified</h3>
@@ -34,6 +34,6 @@ const TopBox = ({ accepted, rejected, modified, role, currentStatus }) => {
       </div>
     </div>
   );
-};
+}
 
 export default TopBox;
